502a7dac8aea6cd2649a45153bf0ca64d34d3106
jeksterslab <social.jeksterslab@gmail.com>
2021-03-22 02:04:21 +0800

    Distinguish types of estimates.

--- R/dot.R
+++ R/dot.R
@@ -31,25 +31,63 @@
 
 .thetahat <- function(object) {
   # extract all estimates including fixed parameters
-  thetahat_names <- paste0(
+  thetahat.names <- paste0(
     object@ParTable$lhs,
     object@ParTable$op,
     object@ParTable$rhs
   )
+  thetahat.labels <- thetahat.names
   label <- object@ParTable$label
-  thetahat_names <- ifelse(
+  thetahat.labels <- ifelse(
     label == "",
-    yes = thetahat_names,
+    yes = thetahat.labels,
     no = label
   )
-  thetahat <- object@ParTable$est
-  names(thetahat) <- thetahat_names
-  # remove paremeters with op `==`
-  for (i in seq_along(thetahat)) {
+  thetahat.est <- object@ParTable$est
+  thetahat.fixed <- thetahat.def <- thetahat.cin <- thetahat.ceq <- rep(
+    x = NA,
+    times = length(thetahat.est)
+  )
+  for (i in seq_along(thetahat.est)) {
     if (object@ParTable$op[i] == "==") {
-      thetahat[i] <- NA
+      thetahat.labels[i] <- paste0(
+        object@ParTable$lhs[i],
+        "_ceq"
+      )
+      thetahat.ceq[i] <- object@ParTable$lhs[i]
+    }
+    if (object@ParTable$op[i] %in% c(">", "<", ">=", "<=")) {
+      thetahat.labels[i] <- paste0(
+        object@ParTable$lhs[i],
+        "_cin"
+      )
+      thetahat.cin[i] <- object@ParTable$lhs[i]
+    }
+    if (object@ParTable$op[i] == ":=") {
+      thetahat.def[i] <- object@ParTable$lhs[i]
+    }
+    if (object@ParTable$free[i] == 0) {
+      fixed <- TRUE
+      if (object@ParTable$op[i] %in% c(":=", "==", ">", "<", ">=", "<=")) {
+        fixed <- FALSE
+      }
+      if (fixed) {
+        thetahat.fixed[i] <- thetahat.labels[i]
+      }
     }
   }
-  thetahat <- thetahat[stats::complete.cases(thetahat)]
-  return(thetahat)
+  names(thetahat.est) <- thetahat.labels
+  def <- thetahat.def[stats::complete.cases(thetahat.def)]
+  ceq <- thetahat.ceq[stats::complete.cases(thetahat.ceq)]
+  cin <- thetahat.cin[stats::complete.cases(thetahat.cin)]
+  fixed <- thetahat.fixed[stats::complete.cases(thetahat.fixed)]
+  list(
+    est = thetahat.est,
+    names = thetahat.names,
+    labels = thetahat.labels,
+    def = def,
+    ceq = ceq,
+    cin = cin,
+    fixed = fixed
+  )
 }
