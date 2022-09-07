0fc1a1a1bfb287cb6041ce02c011abf3e9ddcf27
jeksterslab <learn.jeksterslab@gmail.com>
2022-09-07 16:32:31 +0000

    Automated build.

--- R/semmcci-print.R
+++ R/semmcci-print.R
@@ -5,6 +5,35 @@
 #' @param digits Integer indicating the number of decimal places to display.
 #' @param ... further arguments.
 #' @return Returns a matrix of estimates, standard errors and confidence intervals.
+#' @examples
+#' library(semmcci)
+#' library(lavaan)
+#'
+#' # Generate Data ------------------------------------------------------------
+#' n <- 1000
+#' x <- rnorm(n = n)
+#' m <- 0.50 * x + rnorm(n = n)
+#' y <- 0.25 * x + 0.50 * m + rnorm(n = n)
+#' data <- data.frame(x, m, y)
+#'
+#' # Fit Model in lavaan ------------------------------------------------------
+#' model <- "
+#'   y ~ cp * x + b * m
+#'   m ~ a * x
+#'   ab := a * b
+#' "
+#' fit <- sem(
+#'   data = data,
+#'   model = model
+#' )
+#'
+#' # Monte Carlo --------------------------------------------------------------
+#' out <- MC(
+#'   fit,
+#'   R = 100L, # use a large value e.g., 20000L for actual research
+#'   alpha = c(0.001, 0.01, 0.05)
+#' )
+#' print(out)
 #' @keywords method
 #' @export
 print.semmcci <- function(x,
@@ -21,6 +50,39 @@ print.semmcci <- function(x,
 #' @param digits Integer indicating the number of decimal places to display.
 #' @param ... further arguments.
 #' @return Returns a matrix of estimates, standard errors and confidence intervals.
+#' @examples
+#' library(semmcci)
+#' library(lavaan)
+#'
+#' # Generate Data ------------------------------------------------------------
+#' n <- 1000
+#' x <- rnorm(n = n)
+#' m <- 0.50 * x + rnorm(n = n)
+#' y <- 0.25 * x + 0.50 * m + rnorm(n = n)
+#' data <- data.frame(x, m, y)
+#'
+#' # Fit Model in lavaan ------------------------------------------------------
+#' model <- "
+#'   y ~ cp * x + b * m
+#'   m ~ a * x
+#'   ab := a * b
+#' "
+#' fit <- sem(
+#'   data = data,
+#'   model = model,
+#'   fixed.x = FALSE
+#' )
+#'
+#' # Monte Carlo --------------------------------------------------------------
+#' output <- MC(
+#'   fit,
+#'   R = 100L, # use a large value e.g., 20000L for actual research
+#'   alpha = c(0.001, 0.01, 0.05)
+#' )
+#'
+#' # Standardized Monte Carlo -------------------------------------------------
+#' out <- MCStd(output)
+#' print(out)
 #' @keywords method
 #' @export
 print.semmcci_std <- function(x,
