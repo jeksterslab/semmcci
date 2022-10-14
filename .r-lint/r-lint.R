16762c9eda19dab2dd698ef77824af0529051f6e
jeksterslab <learn.jeksterslab@gmail.com>
2022-10-14 00:32:22 +0000

    Automated build.

--- .r-lint/r-lint.R
+++ .r-lint/r-lint.R
@@ -1,93 +1,102 @@
-# find root directory
-root <- rprojroot::is_rstudio_project
-# root_folder
-root_folder <- dirname(
-  root$find_file(
-    "project.Rproj"
-  )
-)
-dirs <- list.dirs(path = root_folder, full.names = FALSE, recursive = FALSE)
-styles <- c(
-  "CamelCase",
-  "snake_case",
-  "symbols"
-)
-filetype <- c(
-  ".R",
-  ".Rmd"
-)
-
-styler::style_dir(
-  root_folder,
-  exclude_dirs = c(
-    "packrat",
-    "renv",
-    ".library",
-    ".notes"
-  ),
-  filetype = filetype
-)
-
-cat("\nlatexsrc/r-scripts\n")
-latexsrc <- root$find_file(
-  "latexsrc",
-  "r-scripts"
-)
-lintr::lint_dir(
-  latexsrc,
-  lintr::object_name_linter(
-    styles = styles
-  )
-)
-
-cat("\n.r-*\n")
-dot_r <- dirs[grep(pattern = "^\\.r-.*$", x = dirs)]
 lapply(
-  X = dot_r,
-  FUN = function(path) {
+  X = 1,
+  FUN = function(i) {
+    # find root directory
+    root <- rprojroot::is_rstudio_project
+    # root_folder
+    root_folder <- dirname(
+      root$find_file(
+        "project.Rproj"
+      )
+    )
+    dirs <- list.dirs(
+      path = root_folder,
+      full.names = FALSE,
+      recursive = FALSE
+    )
+    styles <- c(
+      "CamelCase",
+      "snake_case",
+      "symbols"
+    )
+    filetype <- c(
+      ".R",
+      ".Rmd"
+    )
+
+    styler::style_dir(
+      root_folder,
+      exclude_dirs = c(
+        "packrat",
+        "renv",
+        ".library",
+        ".notes"
+      ),
+      filetype = filetype
+    )
+
+    cat("\nlatexsrc/r-scripts\n")
+    latexsrc <- root$find_file(
+      "latexsrc",
+      "r-scripts"
+    )
     lintr::lint_dir(
-      file.path(root_folder, path),
+      latexsrc,
+      lintr::object_name_linter(
+        styles = styles
+      )
+    )
+
+    cat("\n.r-*\n")
+    dot_r <- dirs[grep(pattern = "^\\.r-.*$", x = dirs)]
+    lapply(
+      X = dot_r,
+      FUN = function(path) {
+        lintr::lint_dir(
+          file.path(root_folder, path),
+          lintr::object_name_linter(
+            styles = styles
+          )
+        )
+      }
+    )
+
+    cat("\n.tests-*\n")
+    dot_tests <- dirs[grep(pattern = "^\\.tests-.*$", x = dirs)]
+    lapply(
+      X = dot_tests,
+      FUN = function(path) {
+        lintr::lint_dir(
+          file.path(root_folder, path),
+          lintr::object_name_linter(
+            styles = styles
+          )
+        )
+      }
+    )
+
+    cat("\nvignettes\n")
+    lintr::lint_dir(
+      file.path(root_folder, "vignettes"),
+      lintr::object_name_linter(
+        styles = styles
+      )
+    )
+
+    cat("\ntests\n")
+    lintr::lint_dir(
+      file.path(root_folder, "tests"),
+      lintr::object_name_linter(
+        styles = styles
+      )
+    )
+
+    cat("\nR\n")
+    lintr::lint_dir(
+      file.path(root_folder, "R"),
       lintr::object_name_linter(
         styles = styles
       )
     )
   }
-)
-
-cat("\n.tests-*\n")
-dot_tests <- dirs[grep(pattern = "^\\.tests-.*$", x = dirs)]
-lapply(
-  X = dot_tests,
-  FUN = function(path) {
-    lintr::lint_dir(
-      file.path(root_folder, path),
-      lintr::object_name_linter(
-        styles = styles
-      )
-    )
-  }
-)
-
-cat("\nvignettes\n")
-lintr::lint_dir(
-  file.path(root_folder, "vignettes"),
-  lintr::object_name_linter(
-    styles = styles
-  )
-)
-
-cat("\ntests\n")
-lintr::lint_dir(
-  file.path(root_folder, "tests"),
-  lintr::object_name_linter(
-    styles = styles
-  )
-)
-
-cat("\nR\n")
-lintr::lint_dir(
-  file.path(root_folder, "R"),
-  lintr::object_name_linter(
-    styles = styles
-  )
 )
