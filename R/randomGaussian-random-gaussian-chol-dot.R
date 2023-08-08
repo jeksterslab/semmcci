cd0530d447f60f35aa6b132bd560abd9579309c2
jeksterslab <jeksterslab@users.noreply.github.com>
2023-08-08 15:19:02 +0000

    Automated build.
    Files changed:
    A	.Rbuildignore
    A	.github/linters/.lintr
    A	.setup/build/semmcci.pdf
    A	.setup/build/semmcci_1.1.0.tar.gz
    A	.setup/data-raw/benchmark/benchmark-complete-01.Rds
    A	.setup/data-raw/benchmark/benchmark-complete-02.Rds
    A	.setup/data-raw/benchmark/benchmark-fiml-01.Rds
    A	.setup/data-raw/benchmark/benchmark-fiml-02.Rds
    A	.setup/data-raw/benchmark/benchmark-mi-01.Rds
    A	.setup/data-raw/benchmark/benchmark-mi-02.Rds
    A	.setup/lint/.lintr
    A	CITATION.cff
    M	DESCRIPTION
    A	LICENSE
    A	LICENSE.md
    A	NAMESPACE
    A	R/lavHelper-lav-2-ram-dot.R
    A	R/lavHelper-ram-2-lav-dot.R
    A	R/lavHelper-std-lav-dot.R
    A	R/lavHelper-std-ram-dot.R
    A	R/lavHelper-theta-hat-dot.R
    A	R/linearAlgebra-positive-definite-test-dot.R
    A	R/miHelper-ariv-dot.R
    A	R/miHelper-mi-combine-dot.R
    A	R/miHelper-total-adj-dot.R
    A	R/nBootstrap-ci-format-dot.R
    A	R/nBootstrap-pc-ci-dot.R
    A	R/nBootstrap-pc-probs-dot.R
    A	R/randomGaussian-location-dot.R
    A	R/randomGaussian-random-gaussian-chol-dot.R
    A	R/randomGaussian-random-gaussian-eigen-dot.R
    A	R/randomGaussian-random-gaussian-svd-dot.R
    A	R/randomGaussian-random-gaussian-z-dot.R
    A	README.md
    A	man/MC.Rd
    A	man/MCMI.Rd
    A	man/MCStd.Rd
    A	man/coef.semmcci.Rd
    A	man/confint.semmcci.Rd
    A	man/print.semmcci.Rd
    A	man/semmcci-package.Rd
    A	man/summary.semmcci.Rd
    A	man/vcov.semmcci.Rd
    M	project.Rproj
    A	vignettes/benchmark-complete.Rmd
    A	vignettes/benchmark-fiml.Rmd
    A	vignettes/benchmark-mi.Rmd
    A	vignettes/example-1-simple.Rmd
    A	vignettes/example-2-serial.Rmd
    A	vignettes/example-3-latent.Rmd
    A	vignettes/example-4-1-simple-miss-fiml.Rmd
    A	vignettes/example-4-2-simple-miss-mi.Rmd
    A	vignettes/example-5-composite-reliability.Rmd
    A	vignettes/fig-vignettes-benchmark-unnamed-chunk-16-1.png
    A	vignettes/fig-vignettes-benchmark-unnamed-chunk-17-1.png
    A	vignettes/fig-vignettes-benchmark-unnamed-chunk-21-1.png
    A	vignettes/fig-vignettes-benchmark-unnamed-chunk-22-1.png
    A	vignettes/mcstd-1-std-regression.Rmd
    A	vignettes/mcstd-2-difference-regression-coefficients.Rmd
    A	vignettes/mcstd-3-rsqr.Rmd
    A	vignettes/mcstd-4-difference-regression-coefficients-multigroup.Rmd
    A	vignettes/mcstd-5-difference-rsq-multigroup.Rmd
    A	vignettes/methods-mc-mi.Rmd
    A	vignettes/methods-mc.Rmd
    A	vignettes/vignettes.bib

---
+++ R/randomGaussian-random-gaussian-chol-dot.R
@@ -0,0 +1,23 @@
+#' Generate Random Variates from the Gaussian Distribution
+#' (Cholesky Decomposition)
+#'
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @param Z Numeric matrix.
+#'   `n` by `k` matrix of independent random variates
+#'   from the standard univariate normal distribution
+#'   \eqn{\mathbf{Z}}.
+#' @param chol Object.
+#'   Result of [chol()].
+#'
+#' @return Numeric matrix.
+#'
+#' @family Random Gaussian Functions
+#' @keywords randomGaussian random cholesky internal
+#' @noRd
+.RandomGaussianChol <- function(Z,
+                                chol) {
+  return(
+    Z %*% chol
+  )
+}
