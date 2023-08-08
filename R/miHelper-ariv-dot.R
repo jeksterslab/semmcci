86bb689d97dcff707ee98caf75c0222f74d3d85d
jeksterslab <jeksterslab@users.noreply.github.com>
2023-08-08 17:00:31 +0000

    Automated build.
    Files changed:
    A	.Rbuildignore
    A	.github/linters/.lintr
    A	.setup/build/semmcci.pdf
    A	.setup/build/semmcci_1.1.1.tar.gz
    A	.setup/data-raw/benchmark/benchmark-complete-01.Rds
    A	.setup/data-raw/benchmark/benchmark-complete-02.Rds
    A	.setup/data-raw/benchmark/benchmark-fiml-01.Rds
    A	.setup/data-raw/benchmark/benchmark-fiml-02.Rds
    A	.setup/data-raw/benchmark/benchmark-mi-01.Rds
    A	.setup/data-raw/benchmark/benchmark-mi-02.Rds
    A	.setup/lint/.lintr
    A	CITATION.cff
    M	DESCRIPTION
    A	LICENSE
    A	LICENSE.md
    A	NAMESPACE
    A	R/lavHelper-lav-2-ram-dot.R
    A	R/lavHelper-ram-2-lav-dot.R
    A	R/lavHelper-std-lav-dot.R
    A	R/lavHelper-std-ram-dot.R
    A	R/lavHelper-theta-hat-dot.R
    A	R/linearAlgebra-positive-definite-test-dot.R
    A	R/miHelper-ariv-dot.R
    A	R/miHelper-mi-combine-dot.R
    A	R/miHelper-total-adj-dot.R
    A	R/nBootstrap-ci-format-dot.R
    A	R/nBootstrap-pc-ci-dot.R
    A	R/nBootstrap-pc-probs-dot.R
    A	R/randomGaussian-location-dot.R
    A	R/randomGaussian-random-gaussian-chol-dot.R
    A	R/randomGaussian-random-gaussian-eigen-dot.R
    A	R/randomGaussian-random-gaussian-svd-dot.R
    A	R/randomGaussian-random-gaussian-z-dot.R
    A	README.md
    A	man/MC.Rd
    A	man/MCMI.Rd
    A	man/MCStd.Rd
    A	man/coef.semmcci.Rd
    A	man/confint.semmcci.Rd
    A	man/print.semmcci.Rd
    A	man/semmcci-package.Rd
    A	man/summary.semmcci.Rd
    A	man/vcov.semmcci.Rd
    M	project.Rproj
    A	vignettes/benchmark-complete.Rmd
    A	vignettes/benchmark-fiml.Rmd
    A	vignettes/benchmark-mi.Rmd
    A	vignettes/example-1-simple.Rmd
    A	vignettes/example-2-serial.Rmd
    A	vignettes/example-3-latent.Rmd
    A	vignettes/example-4-1-simple-miss-fiml.Rmd
    A	vignettes/example-4-2-simple-miss-mi.Rmd
    A	vignettes/example-5-composite-reliability.Rmd
    A	vignettes/fig-vignettes-benchmark-unnamed-chunk-16-1.png
    A	vignettes/fig-vignettes-benchmark-unnamed-chunk-17-1.png
    A	vignettes/fig-vignettes-benchmark-unnamed-chunk-21-1.png
    A	vignettes/fig-vignettes-benchmark-unnamed-chunk-22-1.png
    A	vignettes/mcstd-1-std-regression.Rmd
    A	vignettes/mcstd-2-difference-regression-coefficients.Rmd
    A	vignettes/mcstd-3-rsqr.Rmd
    A	vignettes/mcstd-4-difference-regression-coefficients-multigroup.Rmd
    A	vignettes/mcstd-5-difference-rsq-multigroup.Rmd
    A	vignettes/methods-mc-mi.Rmd
    A	vignettes/methods-mc.Rmd
    A	vignettes/vignettes.bib

---
+++ R/miHelper-ariv-dot.R
@@ -0,0 +1,67 @@
+#' Average Relative Increase in Variance
+#'
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @details The average relative increase in variance
+#'   is given by
+#'   \deqn{
+#'     \mathrm{ARIV}
+#'     =
+#'     \left( 1 + M^{-1} \right)
+#'     \mathrm{tr}
+#'     \left(
+#'        \mathbf{V}_{\mathrm{between}}
+#'        \mathbf{V}_{\mathrm{within}}^{-1}
+#'     \right)
+#'   }
+#'
+#' @param between Numeric matrix.
+#'   Covariance between imputations
+#'   \eqn{\mathbf{V}_{\mathrm{between}}}.
+#' @param within Numeric matrix.
+#'   Covariance within imputations
+#'   \eqn{\mathbf{V}_{\mathrm{within}}}.
+#' @param M Positive integer.
+#'   Number of imputations.
+#' @param k Positive integer.
+#'   Number of parameters.
+#'
+#' @return Returns a numeric vector of length one.
+#'
+#' @references
+#'   Li, K. H., Raghunathan, T. E., & Rubin, D. B. (1991).
+#'   Large-sample significance levels from multiply imputed data
+#'   using moment-based statistics and an F reference distribution.
+#'   *Journal of the American Statistical Association*, 86 (416), 1065–1073.
+#'   \doi{10.1080/01621459.1991.10475152}
+#'
+#'   Rubin, D. B. (1987).
+#'   *Multiple imputation for nonresponse in surveys*.
+#'   John Wiley & Sons, Inc.
+#'   \doi{10.1002/9780470316696}
+#'
+#' @family Multiple Imputation Helper Functions
+#' @keywords miHelper combine
+#' @noRd
+.ARIV <- function(between,
+                  within,
+                  M,
+                  k) {
+  return(
+    (
+      (
+        1 + (
+          1 / M
+        )
+      ) * sum(
+        diag(
+          between %*% chol2inv(
+            chol(
+              within
+            )
+          )
+        )
+      )
+    ) / k
+  )
+}
