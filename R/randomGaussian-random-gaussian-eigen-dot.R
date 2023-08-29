2f61dc75deecb1b806c03b699c4493ee1f053101
jeksterslab <jeksterslab@users.noreply.github.com>
2023-08-29 00:11:27 +0000

    Automated build.
    Files changed:
    A	.Rbuildignore
    A	.github/linters/.lintr
    A	.setup/build/semmcci.pdf
    A	.setup/build/semmcci_1.1.2.9000.tar.gz
    A	.setup/latex/bib/quarto.bib
    A	.setup/lint/.lintr
    A	CITATION.cff
    M	DESCRIPTION
    A	LICENSE
    A	LICENSE.md
    A	NAMESPACE
    A	R/lavHelper-lav-2-ram-dot.R
    A	R/lavHelper-ram-2-lav-dot.R
    A	R/lavHelper-std-lav-dot.R
    A	R/lavHelper-std-ram-dot.R
    A	R/lavHelper-theta-hat-dot.R
    A	R/linearAlgebra-positive-definite-test-dot.R
    A	R/miHelper-ariv-dot.R
    A	R/miHelper-mi-combine-dot.R
    A	R/miHelper-total-adj-dot.R
    A	R/nBootstrap-ci-format-dot.R
    A	R/nBootstrap-pc-ci-dot.R
    A	R/nBootstrap-pc-probs-dot.R
    A	R/randomGaussian-location-dot.R
    A	R/randomGaussian-random-gaussian-chol-dot.R
    A	R/randomGaussian-random-gaussian-eigen-dot.R
    A	R/randomGaussian-random-gaussian-svd-dot.R
    A	R/randomGaussian-random-gaussian-z-dot.R
    A	README.md
    A	man/MC.Rd
    A	man/MCMI.Rd
    A	man/MCStd.Rd
    A	man/coef.semmcci.Rd
    A	man/confint.semmcci.Rd
    A	man/print.semmcci.Rd
    A	man/semmcci-package.Rd
    A	man/summary.semmcci.Rd
    A	man/vcov.semmcci.Rd
    M	project.Rproj
    M	tests/testthat/test-semmcci-mc-simple-med-std-defined-none.R
    M	tests/testthat/test-semmcci-mc-simple-med-std-defined.R
    A	vignettes/benchmark-complete.Rmd
    A	vignettes/benchmark-fiml.Rmd
    A	vignettes/benchmark-mi.Rmd
    A	vignettes/example-1-simple.Rmd
    A	vignettes/example-2-serial.Rmd
    A	vignettes/example-3-latent.Rmd
    A	vignettes/example-4-1-simple-miss-fiml.Rmd
    A	vignettes/example-4-2-simple-miss-mi.Rmd
    A	vignettes/example-5-composite-reliability.Rmd
    A	vignettes/fig-vignettes-benchmark-unnamed-chunk-16-1.png
    A	vignettes/fig-vignettes-benchmark-unnamed-chunk-17-1.png
    A	vignettes/fig-vignettes-benchmark-unnamed-chunk-21-1.png
    A	vignettes/fig-vignettes-benchmark-unnamed-chunk-22-1.png
    A	vignettes/mcstd-1-std-regression.Rmd
    A	vignettes/mcstd-2-difference-regression-coefficients.Rmd
    A	vignettes/mcstd-3-rsqr.Rmd
    A	vignettes/mcstd-4-difference-regression-coefficients-multigroup.Rmd
    A	vignettes/mcstd-5-difference-rsq-multigroup.Rmd
    A	vignettes/methods-mc-mi.Rmd
    A	vignettes/methods-mc.Rmd
    A	vignettes/vignettes.bib

---
+++ R/randomGaussian-random-gaussian-eigen-dot.R
@@ -0,0 +1,30 @@
+#' Generate Random Variates from the Gaussian Distribution
+#' (Eigen Decomposition)
+#'
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @param Z Numeric matrix.
+#'   `n` by `k` matrix of independent random variates
+#'   from the standard univariate normal distribution
+#'   \eqn{\mathbf{Z}}.
+#' @param eigen Object.
+#'   Result of [eigen()].
+#'
+#' @return Numeric matrix.
+#'
+#' @family Random Gaussian Functions
+#' @keywords randomGaussian random eigen internal
+#' @noRd
+.RandomGaussianEigen <- function(Z,
+                                 eigen) {
+  return(
+    Z %*% (
+      t(eigen$vectors) * sqrt(
+        pmax(
+          eigen$values,
+          0
+        )
+      )
+    )
+  )
+}
