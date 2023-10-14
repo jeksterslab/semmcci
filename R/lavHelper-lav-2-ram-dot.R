bf76438d1822034eaa874411988274adc79b0920
jeksterslab <jeksterslab@users.noreply.github.com>
2023-10-14 19:52:58 +0000

    Automated build.
    Files changed:
    A	.Rbuildignore
    A	.github/linters/.lintr
    A	.setup/build/semmcci.pdf
    A	.setup/build/semmcci_1.1.3.tar.gz
    A	.setup/latex/bib/quarto.bib
    A	.setup/lint/.lintr
    A	CITATION.cff
    M	DESCRIPTION
    A	LICENSE
    A	LICENSE.md
    A	NAMESPACE
    A	R/lavHelper-lav-2-ram-dot.R
    A	R/lavHelper-ram-2-lav-dot.R
    A	R/lavHelper-std-lav-dot.R
    A	R/lavHelper-std-ram-dot.R
    A	R/lavHelper-theta-hat-dot.R
    A	R/linearAlgebra-positive-definite-test-dot.R
    A	R/miHelper-ariv-dot.R
    A	R/miHelper-mi-combine-dot.R
    A	R/miHelper-total-adj-dot.R
    A	R/nBootstrap-ci-format-dot.R
    A	R/nBootstrap-pc-ci-dot.R
    A	R/nBootstrap-pc-probs-dot.R
    A	R/randomGaussian-location-dot.R
    A	R/randomGaussian-random-gaussian-chol-dot.R
    A	R/randomGaussian-random-gaussian-eigen-dot.R
    A	R/randomGaussian-random-gaussian-svd-dot.R
    A	R/randomGaussian-random-gaussian-z-dot.R
    M	R/semmcci-mc-generic.R
    A	README.md
    A	man/MC.Rd
    A	man/MCGeneric.Rd
    A	man/MCMI.Rd
    A	man/MCStd.Rd
    A	man/coef.semmcci.Rd
    A	man/confint.semmcci.Rd
    A	man/print.semmcci.Rd
    A	man/semmcci-package.Rd
    A	man/summary.semmcci.Rd
    A	man/vcov.semmcci.Rd
    M	project.Rproj
    M	tests/testthat/test-semmcci-mc-simple-med-std-defined-none.R
    M	tests/testthat/test-semmcci-mc-simple-med-std-defined.R
    A	vignettes/benchmark-complete.Rmd
    A	vignettes/benchmark-fiml.Rmd
    A	vignettes/benchmark-mi.Rmd
    A	vignettes/example-1-simple.Rmd
    A	vignettes/example-2-serial.Rmd
    A	vignettes/example-3-latent.Rmd
    A	vignettes/example-4-1-simple-miss-fiml.Rmd
    A	vignettes/example-4-2-simple-miss-mi.Rmd
    A	vignettes/example-5-composite-reliability.Rmd
    A	vignettes/fig-vignettes-benchmark-unnamed-chunk-16-1.png
    A	vignettes/fig-vignettes-benchmark-unnamed-chunk-17-1.png
    A	vignettes/fig-vignettes-benchmark-unnamed-chunk-21-1.png
    A	vignettes/fig-vignettes-benchmark-unnamed-chunk-22-1.png
    A	vignettes/mcstd-1-std-regression.Rmd
    A	vignettes/mcstd-2-difference-regression-coefficients.Rmd
    A	vignettes/mcstd-3-rsqr.Rmd
    A	vignettes/mcstd-4-difference-regression-coefficients-multigroup.Rmd
    A	vignettes/mcstd-5-difference-rsq-multigroup.Rmd
    A	vignettes/methods-mc-mi.Rmd
    A	vignettes/methods-mc.Rmd
    A	vignettes/vignettes.bib

---
+++ R/lavHelper-lav-2-ram-dot.R
@@ -0,0 +1,92 @@
+#' lavaan to RAM matrices
+#'
+#' Converts a list of lavaan matrices to a list of RAM matrices.
+#'
+#' @author Shu Fai Cheung
+#' @param lav_mod A list of lavaan model matrices for one group.
+#' @return A list of A, S, F, and M RAM matrices.
+#'
+#' @family Lavaan Helper Functions
+#' @keywords lavHelper matrix internal
+#' @noRd
+.Lav2RAM <- function(lav_mod) {
+  ov_names <- rownames(lav_mod$theta)
+  lv_names <- rownames(lav_mod$psi)
+  lv_names <- lv_names[!(lv_names %in% ov_names)]
+  all_names <- c(ov_names, lv_names)
+  p <- length(ov_names)
+  q <- length(lv_names)
+  k <- length(all_names)
+
+  # Initialize the matrices
+  a_mat <- matrix(
+    data = 0,
+    nrow = k,
+    ncol = k
+  )
+  colnames(a_mat) <- rownames(a_mat) <- all_names
+  s_mat <- matrix(
+    data = 0,
+    nrow = k,
+    ncol = k
+  )
+  colnames(s_mat) <- rownames(s_mat) <- all_names
+  f_mat <- cbind(
+    diag(p),
+    matrix(
+      data = 0,
+      nrow = p,
+      ncol = q
+    )
+  )
+  colnames(f_mat) <- all_names
+  rownames(f_mat) <- ov_names
+  m_mat <- matrix(
+    data = 0,
+    nrow = 1,
+    ncol = p + q
+  )
+  colnames(m_mat) <- all_names
+
+  # Theta to S
+  theta_names <- rownames(lav_mod$theta)
+  s_mat[theta_names, theta_names] <- lav_mod$theta
+
+  # Psi to S
+  psi_names <- rownames(lav_mod$psi)
+  s_mat[psi_names, psi_names] <- lav_mod$psi
+
+  # Lambda to A
+  lambda_rnames <- rownames(lav_mod$lambda)
+  lambda_cnames <- colnames(lav_mod$lambda)
+  a_mat[lambda_rnames, lambda_cnames] <- lav_mod$lambda
+
+  # Beta to A
+  if (!is.null(lav_mod$beta)) {
+    beta_names <- rownames(lav_mod$beta)
+    a_mat[beta_names, beta_names] <- lav_mod$beta
+  }
+
+  # Nu to M
+  if (!is.null(lav_mod$nu)) {
+    m_mat[, rownames(lav_mod$nu)] <- lav_mod$nu
+  } else {
+    m_mat[, rownames(lav_mod$nu)] <- NA
+  }
+
+  # Alpha to M
+  if (!is.null(lav_mod$alpha)) {
+    m_mat[, rownames(lav_mod$alpha)] <- lav_mod$alpha
+  } else {
+    m_mat[, rownames(lav_mod$alpha)] <- NA
+  }
+
+  return(
+    list(
+      A = a_mat,
+      S = s_mat,
+      F = f_mat,
+      M = m_mat
+    )
+  )
+}
