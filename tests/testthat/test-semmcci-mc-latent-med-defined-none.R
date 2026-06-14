dfe29eab6d46a49610d82c98d0104fb2fb0e0488
jeksterslab <jeksterslab@users.noreply.github.com>
2026-06-14 00:27:43 +0000

    Update src to output generated at 4cf752e

--- tests/testthat/test-semmcci-mc-latent-med-defined-none.R
+++ tests/testthat/test-semmcci-mc-latent-med-defined-none.R
@@ -6,6 +6,16 @@ lapply(
                  tol,
                  text) {
     message(text)
+
+    set.seed(42)
+
+    if (!identical(Sys.getenv("NOT_CRAN"), "true") && !interactive()) {
+      message("CRAN: tests skipped.")
+      # nolint start
+      return(invisible(NULL))
+      # nolint end
+    }
+
     testthat::test_that(
       paste(text, "chol"),
       {
--- tests/testthat/test-semmcci-mc-simple-med-defined-mi-amelia.R
+++ tests/testthat/test-semmcci-mc-simple-med-defined-mi-amelia.R
@@ -8,6 +8,16 @@ lapply(
                  tol,
                  text) {
     message(text)
+
+    set.seed(42)
+
+    if (!identical(Sys.getenv("NOT_CRAN"), "true") && !interactive()) {
+      message("CRAN: tests skipped.")
+      # nolint start
+      return(invisible(NULL))
+      # nolint end
+    }
+
     testthat::test_that(
       paste(text, "mice"),
       {
